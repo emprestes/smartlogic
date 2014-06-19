61366ceffca3a8bdb411a1bbc425264bcba75e3b
Prestes, E. M <neoiron@gmail.com>
2014-06-19 02:24:47 -0300

    Sobrescrito métodos de MalhaLogistica
    Re-escrito: equals, hashCode e toString.
    Implementado serialização.

--- core/src/main/java/domain/model/MalhaLogistica.java
+++ core/src/main/java/domain/model/MalhaLogistica.java
@@ -1,9 +1,12 @@
 package domain.model;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.TreeSet;
 
-public class MalhaLogistica {
+public class MalhaLogistica implements Serializable {
+
+    private static final long serialVersionUID = -1628207881226490697L;
 
     private String nome;
     private Collection<Trecho> trechos = new TreeSet<>();
@@ -32,5 +35,49 @@ public class MalhaLogistica {
 
     public void setTrechos(Collection<Trecho> trechos) {
         this.trechos = trechos;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof MalhaLogistica)) {
+            return false;
+        }
+        MalhaLogistica other = (MalhaLogistica) obj;
+        if (nome == null) {
+            if (other.nome != null) {
+                return false;
+            }
+        } else if (!nome.equals(other.nome)) {
+            return false;
+        }
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("MalhaLogistica [nome=");
+        builder.append(nome);
+        builder.append(", trechos=");
+        builder.append(trechos.size());
+        builder.append("]");
+        return builder.toString();
     }
 }
