abb088acc633e59bc9f92602eb386e2182657965
Prestes, E. M <neoiron@gmail.com>
2014-06-23 02:55:58 -0300

    Criados repositórios
    TrechoRepositoryDAO e MalhaLogisticaRepositoryDAO

--- repository-spring-data-jpa/src/main/java/repository/data/MalhaLogisticaRepositoryDAO.java
+++ repository-spring-data-jpa/src/main/java/repository/data/MalhaLogisticaRepositoryDAO.java
@@ -5,17 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-import repository.MalhaLogisticaDAO;
 import domain.model.MalhaLogistica;
 
-@Repository
-public interface MalhaLogisticaRepositoryDAO extends MalhaLogisticaDAO, JpaRepository<MalhaLogistica, Long> {
+public interface MalhaLogisticaRepositoryDAO extends JpaRepository<MalhaLogistica, Long> {
 
-    final static String FIND_BY_MALHA_QUERY = "SELECT t02 FROM MalhaLogistica t01 WHERE t01.nome LIKE :nameMalha";
+    final static String FIND_BY_MALHA_QUERY = "SELECT t01 FROM MalhaLogistica t01 WHERE t01.nome LIKE :nameMalha";
 
-    @Override
     @Query(FIND_BY_MALHA_QUERY)
     List<MalhaLogistica> findAll(@Param("nameMalha") String nameMalha);
 }
--- repository-spring-data-jpa/src/main/java/repository/data/TrechoRepositoryDAO.java
+++ repository-spring-data-jpa/src/main/java/repository/data/TrechoRepositoryDAO.java
@@ -7,16 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import repository.TrechoDAO;
 import domain.model.Trecho;
 
 @Repository
-public interface TrechoRepositoryDAO extends TrechoDAO, JpaRepository<Trecho, Long> {
+public interface TrechoRepositoryDAO extends JpaRepository<Trecho, Long> {
 
     final static String FIND_BY_MALHA_QUERY = "SELECT t02 FROM MalhaLogistica t01 "
             + "INNER JOIN T01.trechos t02 WHERE t01.nome = :nameMalha";
 
-    @Override
     @Query(FIND_BY_MALHA_QUERY)
     List<Trecho> findAll(@Param("nameMalha") String nameMalha);
 }
