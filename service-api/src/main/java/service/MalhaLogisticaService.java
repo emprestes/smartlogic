d3a3b74d9433707955d8765859cd53a0e733d37e
Prestes, E. M <neoiron@gmail.com>
2014-06-23 04:16:59 -0300

    Criado comportamento para procurar todas as malhas

--- service-api/src/main/java/service/MalhaLogisticaService.java
+++ service-api/src/main/java/service/MalhaLogisticaService.java
@@ -1,5 +1,7 @@
 package service;
 
+import java.util.Collection;
+
 import domain.model.MalhaLogistica;
 
 public interface MalhaLogisticaService {
@@ -7,4 +9,6 @@ public interface MalhaLogisticaService {
     void save(MalhaLogistica domain) throws Exception;
 
     MalhaLogistica getOne(Long id) throws Exception;
+
+    Collection<MalhaLogistica> findAll() throws Exception;
 }
