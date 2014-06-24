e137e5c5aff4e3b3539dc6badb1809ee100d427b
Prestes, E. M <neoiron@gmail.com>
2014-06-24 11:15:52 -0300

    Ajuste de implantação para salvar trechos.

--- service-impl/src/main/java/service/impl/DefaultMalhaLogisticaService.java
+++ service-impl/src/main/java/service/impl/DefaultMalhaLogisticaService.java
@@ -4,10 +4,14 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import repository.data.MalhaLogisticaRepositoryDAO;
+import repository.data.TrechoRepositoryDAO;
 import service.MalhaLogisticaService;
 import domain.model.MalhaLogistica;
+import domain.model.Trecho;
 
 @Component
 public class DefaultMalhaLogisticaService implements MalhaLogisticaService {
@@ -15,14 +19,29 @@ public class DefaultMalhaLogisticaService implements MalhaLogisticaService {
     @Autowired
     private MalhaLogisticaRepositoryDAO dao;
 
+    @Autowired
+    private TrechoRepositoryDAO trechoDAO;
+
     /** {@inheritDoc} */
     public MalhaLogistica getOne(Long id) throws Exception {
         return dao.getOne(id);
     }
 
     /** {@inheritDoc} */
+    @Transactional(propagation = Propagation.REQUIRED)
     public void save(MalhaLogistica domain) throws Exception {
         dao.saveAndFlush(domain);
+
+        final Collection<Trecho> trechos = domain.getTrechos();
+
+        if (trechos != null) {
+            for (Trecho trecho : trechos) {
+                trecho.setMalhaLogistica(domain);
+                trechoDAO.save(trecho);
+            }
+
+            trechoDAO.flush();
+        }
     }
 
     /** {@inheritDoc} */
