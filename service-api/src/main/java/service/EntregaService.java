fe5bdf2c2c66abe1dcc2e02f9f34ee4b91450297
Prestes, E. M <neoiron@gmail.com>
2014-06-24 23:02:36 -0300

    Criado comportamento para cálculo de custo.

--- service-api/src/main/java/service/EntregaService.java
+++ service-api/src/main/java/service/EntregaService.java
@@ -4,6 +4,8 @@ import domain.model.Entrega;
 
 public interface EntregaService {
 
+    Double calcularCusto(String origem, String destino, Double autonomia, Double valorLitro) throws Exception;
+
     Double calcularCusto(String rota, Double autonomia, Double valorLitro) throws Exception;
 
     Double calcularCusto(Entrega entrega) throws Exception;
