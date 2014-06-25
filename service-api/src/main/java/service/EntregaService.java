f29163ce77089b4103425b5f849deec9f61baca1
Prestes, E. M. <neoiron@gmail.com>
2014-06-24 23:28:57 -0300

    Ajustes em comportamentos de cálculo de custo.

--- service-api/src/main/java/service/EntregaService.java
+++ service-api/src/main/java/service/EntregaService.java
@@ -1,12 +1,6 @@
 package service;
 
-import domain.model.Entrega;
-
 public interface EntregaService {
 
     Double calcularCusto(String origem, String destino, Double autonomia, Double valorLitro) throws Exception;
-
-    Double calcularCusto(String rota, Double autonomia, Double valorLitro) throws Exception;
-
-    Double calcularCusto(Entrega entrega) throws Exception;
 }
